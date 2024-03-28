067d3b7f8bc1364ace18f69d3d56d92a61e550fe
furqaan <khadimfurqaan786@gmail.com>
2024-03-28 21:57:49 +0000

    finished all functionality

--- script.js
+++ script.js
@@ -1,19 +1,68 @@
-const board = document.querySelector('.board')
+const board = document.querySelector('.board');
+const slider = document.querySelector('.slider');
+const black = document.querySelector('.black');
+const rainbow = document.querySelector('.rainbow');
+const clear = document.querySelector('.clear');
+const eraser = document.querySelector('.eraser');
+const msg = document.querySelector('.msg');
 
-let cellNum = 50;
 let gridWH = board.offsetWidth;
+let currentColour = 'black';
+let cellNum = slider.value;
+
+function removeBoard(){
+  while(board.lastElementChild){
+    board.removeChild(board.lastElementChild);
+  }
+}
+
+function clearBoard(){
+  clear.addEventListener('click', () => {
+    cell.forEach((e)=> {
+      e.target.style.backgroundColor = 'white';
+    })
+  })
+}
+function colourCheck() {
+  black.addEventListener('click', ()=>{
+    currentColour = 'black';
+  })
+  rainbow.addEventListener('click', ()=>{
+    currentColour = 'rainbow';
+  })
+  eraser.addEventListener('click', ()=>{
+    currentColour = 'white';
+  })
+}
+
+function draw(e){
+  if(currentColour === 'black'){
+  e.target.style.backgroundColor = 'black'
+  }
+  else if(currentColour === 'rainbow'){
+    e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * (255))}, ${Math.random() * (255)}, ${Math.random() * (255)})`
+  }
+  else if(currentColour === 'white'){
+    e.target.style.backgroundColor = `white`
+  }
+}
 
 function drawBoard(){
   for(let i = 0; i < cellNum; i++){
     let row = document.createElement('div')
     row.style.display = 'flex'
     row.style.flexDirection = 'row'
+    row.classList.add('row');
     for(let i = 0; i < cellNum; i++){
       let cell = document.createElement('div')
       cell.classList.add('cell')
-      cell.style.width = `${(gridWH / cellNum) - 2}px`
-      cell.style.height = `${(gridWH / cellNum) - 2}px`
+      cell.style.width = `${(gridWH / cellNum)}px`
+      cell.style.height = `${(gridWH / cellNum)}px`
+      cell.addEventListener('mouseover', draw)
       row.appendChild(cell)
+      clear.addEventListener(('click'), () => {
+        cell.style.backgroundColor = 'white';
+      })
     }
     board.appendChild(row);
   }
@@ -22,6 +71,21 @@ function drawBoard(){
 
 function game(){
   drawBoard();
+  slider.oninput = function(){
+    if (this.value > 40){
+      msg.style.backgroundColor = 'red'
+      msg.textContent = 'please enter a value below 100'
+    }
+    else{
+      msg.backgroundColor = 'none'
+      msg. textContent = ''
+      cellNum = this.value
+      removeBoard();
+      drawBoard();
+    }
+  }
+  colourCheck();
+  clearBoard();
 }
 
 game();
